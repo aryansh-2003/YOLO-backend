0114c6371b487bfbabe51246eed222ceae9df630
aryansh-2003 <dixitsanjeev1975@gmail.com>
2026-02-19 22:26:50 +0530

    done

--- src/app.js
+++ src/app.js
@@ -12,21 +12,12 @@ app.use(cors({
 }))
 app.use(express.json({limit:"16kb"}))
 
-
-// app.post('/credentials',(req,res)=>{
-//     const {name,username} = req.body
-//     console.log(name, username)
-
-//     if(!name || !username) return res.status(400).json({message:"username and name is required"})
-//     res.send({name:name,username:username}).status(200)
-// })
-
-// routes
-
 import userRouter from './routes/user.routes.js'
 import messageRouter from './routes/message.routes.js'
+import healthCheckRouter from './routes/healthcheck.route.js'
 
 
+app.use("/api/v1/health",healthCheckRouter)
 app.use("/api/v1/user",userRouter)
 app.use("/api/v1/message",messageRouter)
 
