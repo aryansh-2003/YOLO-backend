4765e62c5cea53e3cb471470381d3c0697c03e58
aryansh-2003 <dixitsanjeev1975@gmail.com>
2026-05-03 17:47:13 +0530

    fixed a login bug

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import cors from 'cors'
 const app = express()
 
 app.use(cors({
-    origin: ["http://localhost:5173","https://yolo-virid-six.vercel.app","https://uptime.betterstack.com"], 
+    origin: ["http://localhost:5173","https://yolo-virid-six.vercel.app","https://uptime.betterstack.com","https://buildwithai-five.vercel.app"], 
     credentials: true
 }))
 app.use(express.json({limit:"16kb"}))
@@ -14,10 +14,14 @@ app.use(express.json({limit:"16kb"}))
 import userRouter from './routes/user.routes.js'
 import messageRouter from './routes/message.routes.js'
 import healthCheckRouter from './routes/healthcheck.route.js'
+import directMessageRouter from './routes/directMessage.routes.js'
+
 
 
 app.use("/api/v1/health",healthCheckRouter)
 app.use("/api/v1/user",userRouter)
 app.use("/api/v1/message",messageRouter)
+app.use("/api/v1/directMessage",directMessageRouter)
+
 
 export default app;
